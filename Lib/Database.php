ebfe93332afb3d06f6e71959cf7eaa2348c731f1
Trey Dickson <zerodragoon@Treys-MacBook-Air.local>
2012-09-30 21:50:40 -0400

    Database file

--- Lib/Database.php
+++ Lib/Database.php
@@ -1,22 +1,25 @@
 <?php 
 
 //Call the Enterprise PHP library
-require_once ('soapclient/SforceEnterpriseClient.php');
-include('lib/Constants.php');
+include('Constants.php');
 
 //Class for logging into and access the database
 class Database {
 	
 	//Static login variables
-	private static $USERNAME = Const->$USERNAME;
-	private static $PASSWORD = Const->$PASSWORD;
-	private static $SECURITY_TOKEN = Const->$SECURITY_TOKEN;
+	private static $USERNAME;
+	private static $PASSWORD;
+	private static $SECURITY_TOKEN;
 	
 	public static function login() {
+	
+		$USERNAME = Constant::getUsername();
+		$PASSWORD = Constant::getPassword();
+		$SECURITY_TOKEN = Constant::getToken();
 		
 		//Connect and login to the database
 		$mySforceConnection = new SforceEnterpriseClient();
-		$mySforceConnection->createConnection('enterprise.wsdl.xml');
+		$mySforceConnection->createConnection('../enterprise.wsdl.xml');
 		$mySforceConnection->login(self::$USERNAME, self::$PASSWORD.self::$SECURITY_TOKEN);
 		
 		//Return the connection for use
