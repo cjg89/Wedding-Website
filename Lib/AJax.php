a015cb9832364681cec6fb20356055445ac83185
Trey Dickson <zerodragoon@Treys-MacBook-Air.local>
2012-10-03 19:13:44 -0400

    Finish Jquery and Ajax

--- Lib/AJax.php
+++ Lib/AJax.php
@@ -11,7 +11,7 @@
 				 //Call the delete record function
 				 return getReservations($code);
 			 } else if ($type == 'attendees') {
-				 return updateAllergies($attendees, $comments, $allergies);
+				 return updateAllergies($code, $attendees, $comments, $allergies);
 			 }
 		}
 	}
@@ -31,7 +31,7 @@
 			$id = $records[0]->Id;
 			 
 			 //Query String for All Menu Items		
-			$query = "Select ID, Name, RSVP__c FROM Attendee__c WHERE Group__c = '" . $id . "'";
+			$query = "Select ID, Name FROM Attendee__c WHERE Group__c = '" . $id . "'";
 			
 			//Query the database
 			$results = $mySforceconnection->query(($query));
@@ -64,11 +64,81 @@
 	
 	}
 	
-	function updateAllergies($attendees, $comments, $allergies) {
-		return $attendees;
+	function updateAllergies($code, $attendees, $comments, $allergies) {
+		try {
+			$attendees = json_decode($attendees, true);
+			//return print_r($attendees);
+			
+			//Build array to push to salesforce
+			$updateArray = Array();
+			
+			foreach($attendees as $key =>$attendee) {
+				//Define a standard object
+				$attendeeUp = new stdClass();
+				$attendeeUp->ID = $key;
+				if($attendee == 'Yes') {
+					$attendeeUp->RSVP__c = true;
+				} else {
+					$attendeeUp->RSVP__c = false;
+				}
+				
+				array_push($updateArray, $attendeeUp);
+
+			}
+			
+			$mySforceconnection = Database::login();
+				
+			$response = $mySforceconnection->update($updateArray, 'Attendee__c');
+			
+			$query = "Select ID FROM Group__c WHERE Name = '" . addslashes($code) . "'";
+			
+			$results = $mySforceconnection->query(($query));
+			$records = $results->records;
+			
+			$id = $records[0]->Id;
+			
+			$group = new stdClass();
+			$group->ID = $id;
+			$group->Food_Allergies__c = $allergies;
+			$group->Comments__c = $comments;
+			
+			//Build array to push to salesforce
+			$updateArray = Array();
+			
+			array_push($updateArray, $group);
+			
+			$response2 = $mySforceconnection->update($updateArray, 'Group__c');
+			
+			//Logout of the database
+			Database::logout($mySforceconnection);
+			
+			foreach($response2 as $response) {
+				if(strcmp($response->success, '1') == 0) {
+					
+				} else {
+					return "Failure";
+				}
+			}
+			
+			foreach($response as $res) {
+				if(strcmp($response->success, '1') == 0) {
+					
+				} else {
+					return "Failure";
+				}
+			}
+			
+			return "Success";
+			
+		} catch(Exception $e) {
+			$error = $e->getMessage();
+			//Logout of the database
+			Database::logout($mySforceconnection);
+			return $error;
+		}
 		
 	}
 	
-	echo ajaxCall(trim($_REQUEST['code']), json_decode($_REQUEST['attendees']), trim($_REQUEST['comments']), trim($_REQUEST['allergies']), trim($_REQUEST['type']));
+	echo ajaxCall(trim($_REQUEST['code']), $_REQUEST['attendees'], trim($_REQUEST['comments']), trim($_REQUEST['allergies']), trim($_REQUEST['type']));
 
 ?>
